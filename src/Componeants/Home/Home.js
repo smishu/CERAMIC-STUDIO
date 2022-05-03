411c044ca9165b383dc6f2a0df78b6d1ab82b2ab
Abraham851 <smishu851@gmail.com>
2022-05-03 20:21:07 +0600

    footer & recat rouder add

---
+++ src/Componeants/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+
+            <h1>This is my site</h1>
+
+        </div>
+    );
+};
+
+export default Home;
